7752d64bb2f276ce76d4bc4829e7b46516ea0167
s-sucharita <sucharita5991@gmail.com>
2026-01-08 15:06:19 +0530

    backend done

---
+++ server/routes/taskRoutes.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const auth = require("../middleware/authMiddleware");
+const { getTasks, addTask, updateTask, deleteTask } = require("../controllers/taskController");
+
+router.get("/", auth, getTasks);
+router.post("/", auth, addTask);
+router.put("/:id", auth, updateTask);
+router.delete("/:id", auth, deleteTask);
+
+module.exports = router;
